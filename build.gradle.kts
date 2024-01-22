3a04ac0f5cf2baa7d27cbb676e574d629668d4bc
Dongeon Kim <kde713@gmail.com>
2024-01-23 01:02:23 +0900

    Bump version (0.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ai.tryalign"
-version = "0.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -20,6 +20,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("com.connectrpc:connect-kotlin-okhttp:0.4.0")
+    implementation("com.connectrpc:connect-kotlin-google-java-ext:0.4.0")
+    implementation("com.google.protobuf:protobuf-java:3.25.2")
     implementation("build.buf.gen:impaction-ai_ingestion_connectrpc_kotlin:0.4.0.1.20230724010212.6e96e3f59731")
     implementation("build.buf.gen:impaction-ai_ingestion_protocolbuffers_kotlin:25.2.0.1.20230724010212.6e96e3f59731")
 }
@@ -44,7 +46,7 @@ publishing {
     }
     publications {
         register<MavenPublication>("gpr") {
-            from(components["java"])
+            from(components["kotlin"])
         }
     }
 }
